35cd2277668cc624ae4e7ed43149d23adcc26558
abrunello <brunello@bludata.com>
2022-01-26 15:39:50 +0100

    from react router dom version 5 to version 6 differences

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,20 +1,35 @@
-import { NavLink } from "react-router-dom"
-import classes from './MainHeader.module.css'
+import { NavLink } from "react-router-dom";
+import classes from "./MainHeader.module.css";
 
-const MainHeader = ()=> {
-
-    return <header className={classes.header}>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink activeClassName={classes.active} to="/welcome">Welcome</NavLink>
-                </li>
-                <li>
-                    <NavLink activeClassName={classes.active} to="/products">Products</NavLink>
-                </li>
-            </ul>
-        </nav>
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? classes.active : null
+              }
+              to="/welcome"
+            >
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? classes.active : null
+              }
+              to="/products"
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
-}
+  );
+};
 
-export default MainHeader
+export default MainHeader;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,13 @@
-import { Route } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <section>
       <h1>Welcome page</h1>
-      <Route path="/welcome/new-user">
-        <h3>Hello new user!</h3>
-      </Route>
+      <Link to="new-user"> New user </Link>
+      <Routes>
+        <Route path="new-user" element={<h3>Hello new user!</h3>} />
+      </Routes>
     </section>
   );
 };
